9058bd13529ff1f7895e6b8dabe3977583b77623
Hubert Woźniak <hubert.wozniak977@gmail.com>
2024-10-02 20:09:33 +0200

    refactor/NEXI-18

---
+++ src/app/mocks/Dashboard.tsx
@@ -0,0 +1,10 @@
+export const dashboardData = [
+  { name: "Sekcja 1" },
+  { name: "Sekcja 2" },
+  { name: "Sekcja 3" },
+  { name: "Sekcja 4" },
+  { name: "Sekcja 5" },
+  { name: "Sekcja 6" },
+  { name: "Sekcja 7" },
+  { name: "Sekcja 8" },
+];
