0e9072f79a5d375d5cd2b9bf48d968a6b114910e
Hubert Woźniak <hubert.wozniak977@gmail.com>
2024-10-15 18:39:12 +0200

    feature/NEXI-21

---
+++ src/app/dashboard-hrm/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { dashboardData } from "../mocks/Dashboard";
+import { Section } from "../components/Section/Section";
+import { NavBar } from "../components/NavBar/NavBar";
+export default function Dashboard() {
+  return (
+    <div className="flex h-screen bg-gradient-to-b bg-oceanBlue to-blue-800">
+      <main className="flex-1 p-8 overflow-y-auto">
+        <div className="grid grid-rows-4 grid-cols-2 gap-4 h-full">
+          {dashboardData.map(({ name }) => (
+            <Section name={name} />
+          ))}
+        </div>
+      </main>
+      <NavBar />
+    </div>
+  );
+}
